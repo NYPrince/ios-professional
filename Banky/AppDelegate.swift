854a89ea40e0ef1ae25e709341cb98b4b8d4ae79
Rick <risingsun66@gmail.com>
2024-02-03 14:43:41 -0500

    Added onboarding view controller

--- Banky/AppDelegate.swift
+++ Banky/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window?.makeKeyAndVisible()
     window?.backgroundColor = .systemBackground
     window?.rootViewController = LoginViewController()
+    window?.rootViewController = OnboardingContainerViewController()
     
     return true
   }
